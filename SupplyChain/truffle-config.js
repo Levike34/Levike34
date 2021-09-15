12ed4427e48efc40ff96991ad28f8c0e1f79d42c
Levike34 <89289513+Levike34@users.noreply.github.com>
2021-09-15 14:22:30 +0900

    Add files via upload

---
+++ SupplyChain/truffle-config.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const path = require("path");
+const mnemonic = "comic wonder member caution insect goat metal above blanket reward game close";
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    ropsten:{
+      provider: function() {
+      return new HDWalletProvider(mnemonic,
+      "https://ropsten.infura.io/v3/1efd3762721044d880f2cf78a782d08e")
+      },
+      network_id: 3
+    },
+  },
+  compilers:{ 
+    solc: { 
+      version: "^0.8.0",
+      optimizer: { 
+    enabled: true, 
+    runs: 200,
+    }}}
+};
+
