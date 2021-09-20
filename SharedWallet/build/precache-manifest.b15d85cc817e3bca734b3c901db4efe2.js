1cfe4637465eedeef0e9ae42dc5b457e87e0a94a
Levike34 <89289513+Levike34@users.noreply.github.com>
2021-09-20 18:46:19 +0900

    Add files via upload

---
+++ SharedWallet/build/precache-manifest.b15d85cc817e3bca734b3c901db4efe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14648f7bcc4666a50a90dfa18c2d839",
+    "url": "/SharedWallet/index.html"
+  },
+  {
+    "revision": "ed4235e905e9c9157d9a",
+    "url": "/SharedWallet/static/css/main.0892298f.chunk.css"
+  },
+  {
+    "revision": "d72fbc68a27b6c9e9ef1",
+    "url": "/SharedWallet/static/js/2.8104a1e5.chunk.js"
+  },
+  {
+    "revision": "ed4235e905e9c9157d9a",
+    "url": "/SharedWallet/static/js/main.b7584204.chunk.js"
+  },
+  {
+    "revision": "e1d0d4d54190fa02e1c1",
+    "url": "/SharedWallet/static/js/runtime-main.9c159c29.js"
+  }
+]);
