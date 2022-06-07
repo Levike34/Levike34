05c5043cc5feb12898aa90d03f4a453281fb569d
Levike34 <89289513+Levike34@users.noreply.github.com>
2022-06-07 10:10:54 +0900

    Add files via upload

---
+++ metaswap/build/precache-manifest.a3001ddb3bc145472a4e25dc5a70458b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07665cef6bdce2cb6d3b72c2806c740",
+    "url": "/home2/iyeszdmy/public_html/vrmetaengine/wp-content/reactpress/apps/metaswap/index.html"
+  },
+  {
+    "revision": "6537565ace56fa685b39",
+    "url": "/home2/iyeszdmy/public_html/vrmetaengine/wp-content/reactpress/apps/metaswap/static/css/main.0b30aec0.chunk.css"
+  },
+  {
+    "revision": "0b30caf55d647f238f4e",
+    "url": "/home2/iyeszdmy/public_html/vrmetaengine/wp-content/reactpress/apps/metaswap/static/js/2.aa01d6d7.chunk.js"
+  },
+  {
+    "revision": "6537565ace56fa685b39",
+    "url": "/home2/iyeszdmy/public_html/vrmetaengine/wp-content/reactpress/apps/metaswap/static/js/main.7a08974c.chunk.js"
+  },
+  {
+    "revision": "e9c084e4e3fbc96fc8e6",
+    "url": "/home2/iyeszdmy/public_html/vrmetaengine/wp-content/reactpress/apps/metaswap/static/js/runtime-main.4c554b87.js"
+  }
+]);
